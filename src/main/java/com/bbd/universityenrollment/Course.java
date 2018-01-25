2013eb17ae4e0db82c0ce90f677f2c11f33bc67c
Mandla Mbuli <mandla@bbd.co.za>
2018-01-25 08:05:52 +0200

    Add tests for Course

--- src/main/java/com/bbd/universityenrollment/Course.java
+++ src/main/java/com/bbd/universityenrollment/Course.java
@@ -1,13 +1,47 @@
 package com.bbd.universityenrollment;
 
-public class Course {
-    private String name;
+import java.util.List;
 
-    public Course(String name) {
+public class Course {
+    private Integer capacity;
+    private String name;
+    private List<String> students;
+    private Integer numberOfCourses;
+
+    public Course(String name, Integer capacity) {
         this.name = name;
+        this.capacity = capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<String> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<String> students) {
+        this.students = students;
+    }
+
+    public Integer getNumberOfCourses() {
+        return numberOfCourses;
+    }
+
+    public void setNumberOfCourses(Integer numberOfCourses) {
+        this.numberOfCourses = numberOfCourses;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
     }
 }
--- src/test/java/com/bbd/universityenrollment/AppTest.java
+++ src/test/java/com/bbd/universityenrollment/AppTest.java
@@ -7,13 +7,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
     @Test
     public void shouldCreateStudent() {
-        Student johnny = new Student("Johnny");
+        Student johnny = new Student("Johnny", 1, "1923-02-07");
         assertEquals(johnny.getName(), "Johnny");
+        assertEquals(johnny.getStudentNumber(), 1);
+        assertEquals(johnny.getDob(), "1923-02-07");
+
+        johnny.setName("Dukey");
+        johnny.setStudentNumber(2);
+        johnny.setDob("Changed");
+
+        assertEquals(johnny.getName(), "Dukey");
+        assertEquals(johnny.getStudentNumber(), 2);
+        assertEquals(johnny.getDob(), "Changed");
     }
 
     @Test
     public void shouldCreateCourse() {
-        Course course = new Course("Test");
+        Course course = new Course("Test", 1);
         assertEquals(course.getName(), "Test");
+        assertEquals(course.getCapacity(), (Integer) 1);
+
+        course.setName("Some other name");
+        course.setCapacity(2);
+        assertEquals(course.getName(), "Some other name");
+        assertEquals(course.getCapacity(), (Integer) 2);
     }
 }
